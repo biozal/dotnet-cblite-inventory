a2022e8f975c128b9303cd6b01cde54257c31b98
Aaron LaBeau <alabeau@gmail.com>
2023-11-21 07:51:51 -0600

    fixing flyout

--- src/Dotnet.Cblite.Inventory.Maui/AppShell.xaml.cs
+++ src/Dotnet.Cblite.Inventory.Maui/AppShell.xaml.cs
@@ -1,23 +1,41 @@
-﻿using Dotnet.Cblite.Inventory.Maui.Views;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Dotnet.Cblite.Inventory.Maui.Views;
+using Dotnet.Cblite.Inventory.Shared.Messages;
 using Dotnet.Cblite.Inventory.Shared.Services;
 
 namespace Dotnet.Cblite.Inventory.Maui;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-		
-		//setup routes
-		Routing.RegisterRoute("projects", typeof(ProjectsView));
-		Routing.RegisterRoute("project", typeof(ProjectView));
-		Routing.RegisterRoute("audits", typeof(AuditsView));
-		Routing.RegisterRoute("audit", typeof(AuditView));
-		Routing.RegisterRoute("developerMenu", typeof(DeveloperMenuView));
-		Routing.RegisterRoute("developerInfo", typeof(DeveloperInfoView));
-		Routing.RegisterRoute("developerLogs", typeof(DeveloperLogsView));
-		Routing.RegisterRoute("userProfile", typeof(UserProfileView));
-		Routing.RegisterRoute("replicator", typeof(ReplicatorView));
-	}
+    private readonly IAuthenticationService _authenticationService;
+
+    public AppShell(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+
+        InitializeComponent();
+
+        //setup routes
+        Routing.RegisterRoute("Projects", typeof(ProjectsView));
+        Routing.RegisterRoute("Project", typeof(ProjectView));
+        Routing.RegisterRoute("Audits", typeof(AuditsView));
+        Routing.RegisterRoute("Audit", typeof(AuditView));
+        Routing.RegisterRoute("DeveloperMenu", typeof(DeveloperMenuView));
+        Routing.RegisterRoute("DeveloperInfo", typeof(DeveloperInfoView));
+        Routing.RegisterRoute("DeveloperLogs", typeof(DeveloperLogsView));
+        Routing.RegisterRoute("UserProfile", typeof(UserProfileView));
+        Routing.RegisterRoute("Replicator", typeof(ReplicatorView));
+    }
+
+    private void Logout_Clicked(object sender, EventArgs e)
+    {
+        FlyoutIsPresented = false; 
+        
+        WeakReferenceMessenger.Default.Send(
+            new AuthenticationMessage(
+                new UserAuthenticationStatus(AuthenticationStatus.SignedOut,
+                    _authenticationService.CurrentUser?.Username ?? string.Empty)
+            )
+        );
+    }
 }
