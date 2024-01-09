70183dc7ef81231dc3f2da8ffe444f5cbf67e41e
Aaron LaBeau <alabeau@gmail.com>
2024-01-08 18:54:47 -0600

    updated to include data loader program

---
+++ Dotnet.Cblite.Inventory.DataLoader/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string jsonFileLocation = "../automation/data-gen";
+const string jsonOrdersFileLocation = "../automation/data-gen/orders";
+
+Console.WriteLine(jsonFileLocation);
+                                             
+Console.ReadKey();
